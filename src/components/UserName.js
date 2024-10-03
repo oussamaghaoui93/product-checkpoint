c7deefafd803a4d8c1c5474154a147359013d667
oussamaghaoui93 <ghaouioussamafst@gmail.com>
2024-10-03 10:25:33 +0100

    second  commit

--- src/components/UserName.js
+++ src/components/UserName.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Alert } from 'react-bootstrap'
-const firstName = 'oussama'; // Change this to your name or leave empty
+import avatar from './7309681.jpg'
 
 const UserName = () => {
+    const firstName = 'Oussama'; // Change this to your name or leave empty
     return (
         <div> <Alert key='warning' variant='warning'>
             <h3>{firstName ? `Hello, ${firstName}!` : 'Hello, there!'}</h3>
-        </Alert></div>
+        </Alert>
+            {firstName && (
+                <img
+                    src={avatar}
+                    alt="Success icon"
+                    style={{ width: '50px', marginLeft: '10px' }}
+                />
+            )}</div>
     )
 }
 
